e7f76c2b743c683a5887415d4eecf48d8b9f9523
antoine.treboutte <antoine.treboutte@epitech.eu>
2020-02-24 20:46:58 +0100

    [ADD] tests for component 4011

--- catch_tests/testComp4011.cpp
+++ catch_tests/testComp4011.cpp
@@ -2,16 +2,16 @@
 ** EPITECH PROJECT, 2019
 ** NanoTekSpice
 ** File description:
-** Tests for 4001 component
+** Tests for 4011 component
 */
 
 #include "catch.hpp"
-#include "components/Comp4001.hpp"
+#include "components/Comp4011.hpp"
 #include "components/IComponents.hpp"
 
-TEST_CASE("Test component 4001 integrity and basic methods")
+TEST_CASE("Test component 4011 integrity and basic methods")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     for (size_t i = 1; i < component.getPins().size() + 1; ++i) {
         REQUIRE(component.getPin(i).getState() == nts::UNDEFINED);
@@ -30,9 +30,9 @@ TEST_CASE("Test component 4001 integrity and basic methods")
 
 // ========= OUPUT 3 TESTS =========
 
-TEST_CASE("Simple NOR with output 3 : !(1 | 1) => 0")
+TEST_CASE("Simple NOR with output 3 : !(1 & 1) => 0")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(1).setState(nts::TRUE);
     component.getPin(2).setState(nts::TRUE);
@@ -40,29 +40,29 @@ TEST_CASE("Simple NOR with output 3 : !(1 | 1) => 0")
     REQUIRE(component.getPin(3).getState() == nts::FALSE);
 }
 
-TEST_CASE("Simple NOR with output 3 : !(1 | 0) => 0")
+TEST_CASE("Simple NOR with output 3 : !(1 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(1).setState(nts::TRUE);
     component.getPin(2).setState(nts::FALSE);
     component.compute(3);
-    REQUIRE(component.getPin(3).getState() == nts::FALSE);
+    REQUIRE(component.getPin(3).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with ouput 3 : !(0 | 1) => 0")
+TEST_CASE("Simple NOR with ouput 3 : !(0 & 1) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(1).setState(nts::FALSE);
     component.getPin(2).setState(nts::TRUE);
     component.compute(3);
-    REQUIRE(component.getPin(3).getState() == nts::FALSE);
+    REQUIRE(component.getPin(3).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with output 3 : !(0 | 0) => 1")
+TEST_CASE("Simple NOR with output 3 : !(0 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(1).setState(nts::FALSE);
     component.getPin(2).setState(nts::FALSE);
@@ -74,9 +74,9 @@ TEST_CASE("Simple NOR with output 3 : !(0 | 0) => 1")
 
 // ========= OUPUT 4 TESTS =========
 
-TEST_CASE("Simple NOR with output 4 : !(1 | 1) => 0")
+TEST_CASE("Simple NOR with output 4 : !(1 & 1) => 0")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(5).setState(nts::TRUE);
     component.getPin(6).setState(nts::TRUE);
@@ -84,29 +84,29 @@ TEST_CASE("Simple NOR with output 4 : !(1 | 1) => 0")
     REQUIRE(component.getPin(4).getState() == nts::FALSE);
 }
 
-TEST_CASE("Simple NOR with output 4 : !(1 | 0) => 0")
+TEST_CASE("Simple NOR with output 4 : !(1 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(5).setState(nts::TRUE);
     component.getPin(6).setState(nts::FALSE);
     component.compute(4);
-    REQUIRE(component.getPin(4).getState() == nts::FALSE);
+    REQUIRE(component.getPin(4).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with ouput 4 : !(0 | 1) => 0")
+TEST_CASE("Simple NOR with ouput 4 : !(0 & 1) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(5).setState(nts::FALSE);
     component.getPin(6).setState(nts::TRUE);
     component.compute(4);
-    REQUIRE(component.getPin(4).getState() == nts::FALSE);
+    REQUIRE(component.getPin(4).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with output 4 : !(0 | 0) => 1")
+TEST_CASE("Simple NOR with output 4 : !(0 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(5).setState(nts::FALSE);
     component.getPin(6).setState(nts::FALSE);
@@ -118,9 +118,9 @@ TEST_CASE("Simple NOR with output 4 : !(0 | 0) => 1")
 
 // ========= OUPUT 10 TESTS =========
 
-TEST_CASE("Simple NOR with output 10 : !(1 | 1) => 0")
+TEST_CASE("Simple NOR with output 10 : !(1 & 1) => 0")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(8).setState(nts::TRUE);
     component.getPin(9).setState(nts::TRUE);
@@ -128,29 +128,29 @@ TEST_CASE("Simple NOR with output 10 : !(1 | 1) => 0")
     REQUIRE(component.getPin(10).getState() == nts::FALSE);
 }
 
-TEST_CASE("Simple NOR with output 10 : !(1 | 0) => 0")
+TEST_CASE("Simple NOR with output 10 : !(1 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(8).setState(nts::TRUE);
     component.getPin(9).setState(nts::FALSE);
     component.compute(10);
-    REQUIRE(component.getPin(10).getState() == nts::FALSE);
+    REQUIRE(component.getPin(10).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with ouput 10 : !(0 | 1) => 0")
+TEST_CASE("Simple NOR with ouput 10 : !(0 & 1) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(8).setState(nts::FALSE);
     component.getPin(9).setState(nts::TRUE);
     component.compute(10);
-    REQUIRE(component.getPin(10).getState() == nts::FALSE);
+    REQUIRE(component.getPin(10).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with output 10 : !(0 | 0) => 1")
+TEST_CASE("Simple NOR with output 10 : !(0 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(8).setState(nts::FALSE);
     component.getPin(9).setState(nts::FALSE);
@@ -162,9 +162,9 @@ TEST_CASE("Simple NOR with output 10 : !(0 | 0) => 1")
 
 // ========= OUPUT 11 TESTS =========
 
-TEST_CASE("Simple NOR with output 11 : !(1 | 1) => 0")
+TEST_CASE("Simple NOR with output 11 : !(1 & 1) => 0")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(12).setState(nts::TRUE);
     component.getPin(13).setState(nts::TRUE);
@@ -172,29 +172,29 @@ TEST_CASE("Simple NOR with output 11 : !(1 | 1) => 0")
     REQUIRE(component.getPin(11).getState() == nts::FALSE);
 }
 
-TEST_CASE("Simple NOR with output 11 : !(1 | 0) => 0")
+TEST_CASE("Simple NOR with output 11 : !(1 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(12).setState(nts::TRUE);
     component.getPin(13).setState(nts::FALSE);
     component.compute(11);
-    REQUIRE(component.getPin(11).getState() == nts::FALSE);
+    REQUIRE(component.getPin(11).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with ouput 11 : !(0 | 1) => 0")
+TEST_CASE("Simple NOR with ouput 11 : !(0 | 1) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(12).setState(nts::FALSE);
     component.getPin(13).setState(nts::TRUE);
     component.compute(11);
-    REQUIRE(component.getPin(11).getState() == nts::FALSE);
+    REQUIRE(component.getPin(11).getState() == nts::TRUE);
 }
 
-TEST_CASE("Simple NOR with output 11 : !(0 | 0) => 1")
+TEST_CASE("Simple NOR with output 11 : !(0 & 0) => 1")
 {
-    nts::Comp4001 component("my_component");
+    nts::Comp4011 component("my_component");
 
     component.getPin(12).setState(nts::FALSE);
     component.getPin(13).setState(nts::FALSE);
--- src/components/Comp4011.cpp
+++ src/components/Comp4011.cpp
@@ -12,13 +12,13 @@ nts::Tristate nts::Comp4011::compute(std::size_t pin)
     nts::Tristate tmp = nts::UNDEFINED;
 
     if (pin == 3)
-        tmp = (nts::Tristate)!(this->IOPins[0].getState() && this->IOPins[1].getState());
+        tmp = (nts::Tristate)!(this->IOPins[0].getState() & this->IOPins[1].getState());
     else if (pin == 4)
-        tmp = (nts::Tristate)!(this->IOPins[4].getState() && this->IOPins[5].getState());
+        tmp = (nts::Tristate)!(this->IOPins[4].getState() & this->IOPins[5].getState());
     else if (pin == 10)
-        tmp = (nts::Tristate)!(this->IOPins[7].getState() && this->IOPins[8].getState());
+        tmp = (nts::Tristate)!(this->IOPins[7].getState() & this->IOPins[8].getState());
     else if (pin == 11)
-        tmp = (nts::Tristate)!(this->IOPins[11].getState() && this->IOPins[12].getState());
+        tmp = (nts::Tristate)!(this->IOPins[11].getState() & this->IOPins[12].getState());
     if (tmp != nts::UNDEFINED && (pin == 3 || pin == 4 || pin == 10 || pin == 11))
         this->IOPins[pin - 1].setState(tmp);
     return (tmp);
